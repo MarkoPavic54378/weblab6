c30fed9f3781b8ab0c7f4354719c5b0daaa20513
MarkoPavic54378 <148389159+MarkoPavic54378@users.noreply.github.com>
2026-02-01 21:14:20 +0100

    Update server.js - remove ping function

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use(express.json({ limit: "1mb" }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/ping", (req, res) => {
+/*app.get("/api/ping", (req, res) => {
   res.json({ ok: true, time: new Date().toISOString() });
-});
+});*/
 app.get("/api/push/public-key", (req, res) => {
   res.json({ key: process.env.VAPID_PUBLIC_KEY || "" });
 });
